6e8934f67974e5108e7066a0aeeeef5d53720d76
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-28 20:23:35 +0200

    create theme

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    whiteLilac: "#FBFBFE",
+    mineShaft: "#252525",
+    transparentMineShaft: "#363636B8",
+    slateGray: "#6E7E91",
+    transparentIron: "#D1D5DA4D",
+    scienceBlue: "#0366D6",
+    anakiwa: "#8CC2FF",
+    parsley: "#14462033",
+    dodgerBlue: "#2188FF",
+    shipCove: "#6D93BE",
+  },
+  size: {
+    medium: 16,
+    large: 20,
+  },
+  breakpoint: {
+    mobileMax: 767,
+  },
+};
