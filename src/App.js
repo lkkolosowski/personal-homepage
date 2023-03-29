4b3349b091e92beb563edc6419807e0de9bef275
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-03-29 02:48:48 +0200

    import section and hero components to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { Container } from "./common/Container";
+import Container from "./common/Container";
+import Hero from "./common/Hero";
+import Section from "./common/Section";
 
 function App() {
-  return <Container>Hello World!</Container>;
+  return (
+    <Container>
+      <Hero />
+      <Section title="My skillset includes" />
+    </Container>
+  );
 }
 
 export default App;
