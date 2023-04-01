07512827b04d416d43efa9dde7bae2369780eed0
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-04-01 23:13:13 +0200

    add theme properties

--- src/theme.js
+++ src/theme.js
@@ -12,8 +12,14 @@ export const theme = {
     dodgerBlue: "#2188FF",
     shipCove: "#6D93BE",
   },
+  shadow: {
+    thin: "0px 4px 12px rgba(186, 199, 213, 0.5)",
+    thick: `0px -2px 50px rgba(9, 10, 51, 0.02),
+    0px 16px 58px rgba(9, 10, 51, 0.03)`,
+  },
   size: {
-    medium: 16,
+    small: 14,
+    medium: 18,
     large: 20,
   },
   breakpoint: {
