ed7317cf2fe53f7d02c1e97eefe74d5368458889
Lukasz Kolosowski <lkkolosowski@gmail.com>
2023-04-04 01:14:48 +0200

    import Contact component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import Container from "./common/Container";
 import Header from "./common/Header";
-import { skills, goals } from "./common/Skillset/nameplate";
+import { skills, goals } from "./nameplate";
 import Skillset from "./common/Skillset";
 import Portfolio from "./common/Portfolio";
+import Contact from "./common/Contact";
 
 function App() {
   return (
     <Container>
       <Header />
-      <Skillset title={<>My skillset includes&nbsp;🛠️</>} skills={skills} />
+      <Skillset
+        title={<>My skillset includes&nbsp;🛠️</>}
+        skills={skills}
+      />
       <Skillset
         title={<>What I want to learn next&nbsp;🚀</>}
         skills={goals}
       />
       <Portfolio />
+      <Contact />
     </Container>
   );
 }
