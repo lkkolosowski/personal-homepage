2a84bd2aa54949f5128210d524569111524a14f6
lkkolosowski <lkkolosowski@gmail.com>
2023-04-18 19:46:04 +0200

    make the button full width

--- src/features/Hero/index.js
+++ src/features/Hero/index.js
@@ -20,7 +20,7 @@ const Hero = () => {
         <Subtitle content={phrase} />
         <Title>{name}</Title>
         <HeaderParagraph>{description}</HeaderParagraph>
-        <ButtonLink title={email} href={`mailto:${email}`}>
+        <ButtonLink wide title={email} href={`mailto:${email}`}>
           <Icon />
           Hire me
         </ButtonLink>
