4e6dd2b61b58c8f479cc1cad5088a01c1176594a
lkkolosowski <lkkolosowski@gmail.com>
2023-04-25 04:08:04 +0200

    add description to skills

--- src/features/Homepage/Skills/skillsData.js
+++ src/features/Homepage/Skills/skillsData.js
@@ -16,42 +16,56 @@ export const skills = [
     Icon: styledIcon(HtmlIcon),
     color: "#008db9",
     level: "✓ Intermediate",
+    description:
+      "I write clean and semantically structured code in accordance with accessibility guidelines, ensuring that every user can access and interact with the content.",
   },
   {
     name: "CSS 3",
     Icon: styledIcon(CssIcon),
     color: "#1572b6",
     level: "✓ Intermediate",
+    description:
+      "I utilize the power of popular CSS frameworks like flexbox and grid to create stunning, responsive layouts that adapt to any screen size and improve the user experience.",
   },
   {
     name: "React Router",
     Icon: styledIcon(ReactRouterIcon),
     color: "#ca4245",
     level: "✓ Basics",
+    description:
+      "I implement React Router to create seamless and dynamic navigation between different pages of your website, allowing users to easily navigate and interact.",
   },
   {
     name: "Redux",
     Icon: styledIcon(ReduxIcon),
     color: "#593d88",
     level: "✓ Basics",
+    description:
+      "I integrate Redux into my React applications to manage complex state logic and ensure that the data flows smoothly between components, resulting in a more scalable and maintainable codebase.",
   },
   {
     name: "React",
     Icon: styledIcon(ReactIcon),
     color: "#20232a",
     level: "✓ Basics",
+    description:
+      "I build dynamic and interactive user interfaces using React, using its powerful component-based architecture to create high-performance and responsive web applications.",
   },
   {
     name: "JS ES6+",
     Icon: styledIcon(JavascriptIcon),
     color: "#323330",
     level: "✓ Intermediate",
+    description:
+      "I'm writing code in JS ES6+  including features like arrow functions, template literals, and destructuring, that is both easy to read and maintain.",
   },
   {
     name: "styled-components",
     Icon: styledIcon(StyledComponentsIcon),
     color: "#db7093",
     level: "✓ Intermediate",
+    description:
+      "I use styled-components to create highly customizable and reusable UI components, allowing me to easily maintain a consistent design system and streamline the development process.",
   },
 ];
 
