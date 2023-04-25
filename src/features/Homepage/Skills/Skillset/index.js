7bcdf0f8b33a82466a664a3f7367cc397c74a5b5
lkkolosowski <lkkolosowski@gmail.com>
2023-04-25 04:10:08 +0200

    add descriptions and styling to Skills

--- src/features/Homepage/Skills/Skillset/index.js
+++ src/features/Homepage/Skills/Skillset/index.js
@@ -7,38 +7,49 @@ import {
   CardFrontInner,
   CardBack,
   CardBackInner,
-  List,
+  Skills,
   Text,
+  Skill,
+  SkillContent,
+  SkillContentInner,
 } from "./styled";
 
 const Skillset = ({ title, icon, subtitle, skills }) => (
   <Section>
     <Header icon={icon} title={title} subtitle={subtitle} />
     <SectionBody>
-      <List>
-        {skills.map(({ name, Icon, color, level }) => (
-          <Card key={name}>
-            <CardFront style={{ backgroundColor: color }}>
-              <CardFrontInner>
-                <Icon />
-                <Text>{name}</Text>
-              </CardFrontInner>
-              <CardBack style={{ backgroundColor: color }}>
-                <CardBackInner>
-                  {level ? (
-                    <Text>{level}</Text>
-                  ) : (
-                    <>
-                      <Icon />
-                      <Text>{name}</Text>
-                    </>
-                  )}
-                </CardBackInner>
-              </CardBack>
-            </CardFront>
-          </Card>
+      <Skills>
+        {skills.map(({ name, Icon, color, level, description }) => (
+          <Skill key={name}>
+            <Card>
+              <CardFront style={{ backgroundColor: color }}>
+                <CardFrontInner>
+                  <Icon enlarged={description ? false : true} />
+                  <Text>{name}</Text>
+                </CardFrontInner>
+                <CardBack style={{ backgroundColor: color }}>
+                  <CardBackInner>
+                    {level ? (
+                      <Text>{level}</Text>
+                    ) : (
+                      <>
+                        <Icon />
+                        <Text>{name}</Text>
+                      </>
+                    )}
+                  </CardBackInner>
+                </CardBack>
+              </CardFront>
+            </Card>
+
+            {description && (
+              <SkillContent>
+                <SkillContentInner>{description}</SkillContentInner>
+              </SkillContent>
+            )}
+          </Skill>
         ))}
-      </List>
+      </Skills>
     </SectionBody>
   </Section>
 );
--- src/features/Homepage/Skills/Skillset/styled.js
+++ src/features/Homepage/Skills/Skillset/styled.js
@@ -1,33 +1,76 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const SectionBody = styled.div`
-  color: ${({ theme }) => theme.textAlternative};
-  transition: color ${({ theme }) => theme.themeAnimation};
-`;
+export const SectionBody = styled.div``;
 
 export const Text = styled.span`
   text-transform: uppercase;
   font-weight: 600;
-  font-size: 16px;
+  font-size: 14px;
   line-height: 1;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
-    font-size: 14px;
-  }
 `;
 
-export const List = styled.ul`
+export const Skills = styled.ul`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
-  gap: 16px;
+  gap: 32px 16px;
   margin-top: 0;
   margin-bottom: 0;
   padding: 0;
   list-style: none;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}px) {
+    gap: 16px;
+  }
 `;
 
-export const Card = styled.li`
+export const Skill = styled.li`
+  width: calc((100% / 4) - (((4-1) / 4) * 16px));
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}px) {
+    width: calc((100% / 2) - (((2-1) / 2) * 16px));
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    width: 100%;
+  }
+`;
+
+export const SkillContent = styled.div`
+  margin-top: -25px;
+  padding-top: 25px;
+  border-radius: 4px;
+  background-color: ${({ theme }) => theme.tile.background};
+  color: ${({ theme }) => theme.textAlternative};
+  transition: border-color ${({ theme }) => theme.themeAnimation},
+    background-color ${({ theme }) => theme.themeAnimation},
+    color ${({ theme }) => theme.themeAnimation};
+  border: 3px solid ${({ theme }) => theme.tile.border};
+  font-size: 16px;
+  line-height: 1.5;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    margin-top: -24px;
+    padding-top: 24px;
+    font-size: 14px;
+  }
+`;
+
+export const SkillContentInner = styled.div`
+  height: calc(13.5em + 50px);
+  padding: 25px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.large}px) {
+    height: calc(10.5em + 50px);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
+    height: auto;
+    padding: 24px;
+  }
+`;
+
+export const Card = styled.div`
   perspective: 800px;
   cursor: default;
 
@@ -65,11 +108,7 @@ export const CardFrontInner = styled.div`
   align-items: center;
   transition: background-color ${({ theme }) => theme.themeAnimation},
     box-shadow ${({ theme }) => theme.themeAnimation};
-  padding: 11px 48px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
-    padding: 11px 32px;
-  }
+  padding: 9px 12px;
 `;
 
 export const CardBack = styled.div`
@@ -106,10 +145,23 @@ export const CardBackInner = styled.div`
 export const styledIcon = (Icon) => styled(Icon)`
   width: 28px;
   height: 28px;
+  margin: 2px 0;
+
+  ${({ enlarged }) =>
+    enlarged &&
+    css`
+      margin: 22px 0;
+    `};
 
   @media (max-width: ${({ theme }) => theme.breakpoint.medium}px) {
     width: 26px;
     height: 26px;
+
+    ${({ enlarged }) =>
+      enlarged &&
+      css`
+        margin: 2px 0;
+      `};
   }
 `;
 
