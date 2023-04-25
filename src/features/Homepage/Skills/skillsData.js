167724e26a1c8ff69efca912b31c01e5afa4daaf
lkkolosowski <lkkolosowski@gmail.com>
2023-04-25 04:35:34 +0200

    modify skills data

--- src/features/Homepage/Skills/skillsData.js
+++ src/features/Homepage/Skills/skillsData.js
@@ -17,7 +17,7 @@ export const skills = [
     color: "#008db9",
     level: "✓ Intermediate",
     description:
-      "I write clean and semantically structured code in accordance with accessibility guidelines, ensuring that every user can access and interact with the content.",
+      "I write clean and semantically structured code in HTML accordance with accessibility guidelines, ensuring that every user can access and interact with the content.",
   },
   {
     name: "CSS 3",
@@ -28,20 +28,20 @@ export const skills = [
       "I utilize the power of popular CSS frameworks like flexbox and grid to create stunning, responsive layouts that adapt to any screen size and improve the user experience.",
   },
   {
-    name: "React Router",
-    Icon: styledIcon(ReactRouterIcon),
-    color: "#ca4245",
-    level: "✓ Basics",
+    name: "styled-components",
+    Icon: styledIcon(StyledComponentsIcon),
+    color: "#db7093",
+    level: "✓ Intermediate",
     description:
-      "I implement React Router to create seamless and dynamic navigation between different pages of your website, allowing users to easily navigate and interact.",
+      "I use styled-components to create highly customizable and reusable UI components, allowing me to easily maintain a consistent design system and streamline the development process.",
   },
   {
-    name: "Redux",
-    Icon: styledIcon(ReduxIcon),
-    color: "#593d88",
-    level: "✓ Basics",
+    name: "JS ES6+",
+    Icon: styledIcon(JavascriptIcon),
+    color: "#323330",
+    level: "✓ Intermediate",
     description:
-      "I integrate Redux into my React applications to manage complex state logic and ensure that the data flows smoothly between components, resulting in a more scalable and maintainable codebase.",
+      "I code in JS ES6+  including features like arrow functions, template literals, and destructuring, that is both easy to read and maintain.",
   },
   {
     name: "React",
@@ -52,20 +52,20 @@ export const skills = [
       "I build dynamic and interactive user interfaces using React, using its powerful component-based architecture to create high-performance and responsive web applications.",
   },
   {
-    name: "JS ES6+",
-    Icon: styledIcon(JavascriptIcon),
-    color: "#323330",
-    level: "✓ Intermediate",
+    name: "Redux",
+    Icon: styledIcon(ReduxIcon),
+    color: "#593d88",
+    level: "✓ Basics",
     description:
-      "I'm writing code in JS ES6+  including features like arrow functions, template literals, and destructuring, that is both easy to read and maintain.",
+      "I integrate Redux into my React applications to manage complex state logic and ensure that the data flows smoothly between components, resulting in a more scalable and maintainable codebase.",
   },
   {
-    name: "styled-components",
-    Icon: styledIcon(StyledComponentsIcon),
-    color: "#db7093",
-    level: "✓ Intermediate",
+    name: "React Router",
+    Icon: styledIcon(ReactRouterIcon),
+    color: "#ca4245",
+    level: "✓ Basics",
     description:
-      "I use styled-components to create highly customizable and reusable UI components, allowing me to easily maintain a consistent design system and streamline the development process.",
+      "I implement React Router to create seamless and dynamic navigation between different pages of your website, allowing users to easily navigate and interact.",
   },
 ];
 
