324c90fb28ddf96da270547b4573db4ce9bd88d1
lkkolosowski <lkkolosowski@gmail.com>
2023-05-17 12:57:09 +0200

    change text to emojis

--- src/features/Homepage/Skills/skillsData.js
+++ src/features/Homepage/Skills/skillsData.js
@@ -15,7 +15,7 @@ export const skills = [
     name: "HTML 5",
     Icon: styledIcon(HtmlIcon),
     color: "#008db9",
-    level: "✓ Intermediate",
+    level: "⭐⭐⭐⭐⭐⭐",
     description:
       "I write clean and semantically structured code in HTML accordance with accessibility guidelines, ensuring that every user can access and interact with the content.",
   },
@@ -23,7 +23,7 @@ export const skills = [
     name: "CSS 3",
     Icon: styledIcon(CssIcon),
     color: "#1572b6",
-    level: "✓ Intermediate",
+    level: "⭐⭐⭐⭐⭐",
     description:
       "I utilize the power of popular CSS frameworks like flexbox and grid to create stunning, responsive layouts that adapt to any screen size and improve the user experience.",
   },
@@ -31,7 +31,7 @@ export const skills = [
     name: "styled-components",
     Icon: styledIcon(StyledComponentsIcon),
     color: "#db7093",
-    level: "✓ Intermediate",
+    level: "⭐⭐⭐⭐⭐",
     description:
       "I use styled-components to create highly customizable and reusable UI components, allowing me to easily maintain a consistent design system and streamline the development process.",
   },
@@ -39,7 +39,7 @@ export const skills = [
     name: "JS ES6+",
     Icon: styledIcon(JavascriptIcon),
     color: "#323330",
-    level: "✓ Intermediate",
+    level: "⭐⭐⭐⭐",
     description:
       "I code in JS ES6+  including features like arrow functions, template literals, and destructuring, that is both easy to read and maintain.",
   },
@@ -47,7 +47,7 @@ export const skills = [
     name: "React",
     Icon: styledIcon(ReactIcon),
     color: "#20232a",
-    level: "✓ Basics",
+    level: "⭐⭐⭐⭐",
     description:
       "I build dynamic and interactive user interfaces using React, using its powerful component-based architecture to create high-performance and responsive web applications.",
   },
@@ -55,7 +55,7 @@ export const skills = [
     name: "Redux",
     Icon: styledIcon(ReduxIcon),
     color: "#593d88",
-    level: "✓ Basics",
+    level: "⭐⭐⭐⭐",
     description:
       "I integrate Redux into my React applications to manage complex state logic and ensure that the data flows smoothly between components, resulting in a more scalable and maintainable codebase.",
   },
@@ -63,7 +63,7 @@ export const skills = [
     name: "React Router",
     Icon: styledIcon(ReactRouterIcon),
     color: "#ca4245",
-    level: "✓ Basics",
+    level: "⭐⭐⭐⭐",
     description:
       "I implement React Router to create seamless and dynamic navigation between different pages of your website, allowing users to easily navigate and interact.",
   },
