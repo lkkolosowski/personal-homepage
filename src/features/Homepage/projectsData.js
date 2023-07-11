749f8398c5ff6a00592585a41ca3a676bcab2e85
lkkolosowski <lkkolosowski@gmail.com>
2023-07-12 00:09:24 +0200

    add icon to individual projects

--- src/features/Homepage/projectsData.js
+++ src/features/Homepage/projectsData.js
@@ -1,11 +1,19 @@
+import image1 from "../../images/arrays-and-objects.png";
+import image2 from "../../images/currency-converter-react.png";
+import image3 from "../../images/food-market.png";
+import image4 from "../../images/homepage.png";
+import image5 from "../../images/movies-browser.png";
+import image6 from "../../images/personal-homepage.png";
+import image7 from "../../images/portfolio-nextjs.png";
+import image8 from "../../images/to-do-list-react.png";
+
 export const projectsData = [
-  { name: "arrays-and-objects", icon: "" },
-  { name: "currency-converter-react", icon: "" },
-  { name: "food-market", icon: "" },
-  { name: "homepage", icon: "" },
-  { name: "movies-browser", icon: "" },
-  { name: "personal-homepage", icon: "" },
-  { name: "portfolio-nextjs", icon: "" },
-  { name: "to-do-list-react", icon: "" },
-  { name: "lkkolosowski", icon: "" },
+  { name: "arrays-and-objects", icon: image1 },
+  { name: "currency-converter-react", icon: image2 },
+  { name: "food-market", icon: image3 },
+  { name: "homepage", icon: image4 },
+  { name: "movies-browser", icon: image5 },
+  { name: "personal-homepage", icon: image6 },
+  { name: "portfolio-nextjs", icon: image7 },
+  { name: "to-do-list-react", icon: image8 },
 ];
