a8627a7d734a8e2432c6a69b8698b2f957a62d73
lkkolosowski <lkkolosowski@gmail.com>
2023-07-11 23:39:19 +0200

    create projects data file

---
+++ src/features/Homepage/projectsData.js
@@ -0,0 +1,11 @@
+export const projectsData = [
+  { name: "arrays-and-objects", icon: "" },
+  { name: "currency-converter-react", icon: "" },
+  { name: "food-market", icon: "" },
+  { name: "homepage", icon: "" },
+  { name: "movies-browser", icon: "" },
+  { name: "personal-homepage", icon: "" },
+  { name: "portfolio-nextjs", icon: "" },
+  { name: "to-do-list-react", icon: "" },
+  { name: "lkkolosowski", icon: "" },
+];
