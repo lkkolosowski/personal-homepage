a7b6c9cc8b0a83235fe31e241b94e600c18c6175
lkkolosowski <lkkolosowski@gmail.com>
2023-07-21 12:10:58 +0200

    bug fix: load images properly

--- src/features/Homepage/Portfolio/Content/Repositories/index.js
+++ src/features/Homepage/Portfolio/Content/Repositories/index.js
@@ -4,46 +4,48 @@ import { Repository, StyledRepositories } from "./styled";
 import { motion } from "framer-motion";
 import { useOnLoadImages } from "./useOnLoadImages";
 import Loader from "../Loader";
+import { transformedRepositories } from "./utils";
 
 const Repositories = ({ repositories }) => {
   const wrapperRef = useRef(null);
   const imagesLoaded = useOnLoadImages(wrapperRef);
 
-  const MotionRepository = motion(Repository);
-
   return (
     <>
       {!imagesLoaded && <Loader />}
       <StyledRepositories ref={wrapperRef} hidden={!imagesLoaded}>
-        {repositories.map(
+        {transformedRepositories(repositories).map(
           ({ id, name, description, homepage, html_url }, i) => (
-            <MotionRepository
-              key={id}
-              initial={{
-                opacity: 0,
-                y: 50,
-              }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-              }}
-              transition={{
-                type: "spring",
-                duration: 0.9,
-                bounce: 0.45,
-                delay: i * 0.1,
-              }}
-              viewport={{
-                once: true,
-              }}
-            >
-              <Tile
-                name={name}
-                description={description}
-                homepage={homepage}
-                html_url={html_url}
-              />
-            </MotionRepository>
+            <Repository key={id}>
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  y: 50,
+                  height: "100%",
+                }}
+                whileInView={{
+                  opacity: 1,
+                  y: 0,
+                  height: "100%",
+                }}
+                transition={{
+                  type: "spring",
+                  duration: 0.9,
+                  bounce: 0.45,
+                  delay: i * 0.1,
+                }}
+                viewport={{
+                  once: true,
+                }}
+              >
+                <Tile
+                  name={name}
+                  description={description}
+                  homepage={homepage}
+                  html_url={html_url}
+                />
+              </motion.div>
+            </Repository>
           )
         )}
       </StyledRepositories>
