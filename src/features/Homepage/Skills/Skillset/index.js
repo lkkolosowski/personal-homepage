5cedebfa2c00f0e53e9bd1514a41d80588a6c148
lkkolosowski <lkkolosowski@gmail.com>
2023-07-22 10:49:25 +0200

    bug fix: render motion divs properly

--- src/features/Homepage/Skills/Skillset/index.js
+++ src/features/Homepage/Skills/Skillset/index.js
@@ -5,56 +5,57 @@ import { Skills, Text, Skill, SkillBody } from "./styled";
 import { motion } from "framer-motion";
 
 const Skillset = ({ title, icon, subtitle, skills }) => {
-  const MotionSkill = motion(Skill);
-
   return (
     <Section>
       <Header icon={icon} title={title} subtitle={subtitle} />
       <Skills>
         {skills.map(({ name, Icon, color, level, description }, i) => (
-          <MotionSkill
-            initial={{
-              opacity: 0,
-              y: 75,
-            }}
-            whileInView={{
-              opacity: 1,
-              y: 0,
-            }}
-            transition={{
-              type: "spring",
-              duration: 0.9,
-              bounce: 0.45,
-              delay: i * 0.1,
-            }}
-            viewport={{
-              once: true,
-            }}
-            key={name}
-          >
-            <Card
-              cardFrontContent={
-                <>
-                  <Icon />
-                  <Text>{name}</Text>
-                </>
-              }
-              cardBackContent={
-                <>
-                  {!level && <Icon />}
-                  <Text>{level ? level : name}</Text>
-                </>
-              }
-              cardFrontBackground={color}
-              cardBackBackground={color}
-              withPadding={true}
-              brightening={true}
-            />
+          <Skill>
+            <motion.div
+              initial={{
+                opacity: 0,
+                y: 75,
+                height: "100%",
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                type: "spring",
+                duration: 0.9,
+                bounce: 0.45,
+                delay: i * 0.1,
+              }}
+              viewport={{
+                once: true,
+              }}
+              key={name}
+            >
+              <Card
+                cardFrontContent={
+                  <>
+                    <Icon />
+                    <Text>{name}</Text>
+                  </>
+                }
+                cardBackContent={
+                  <>
+                    {!level && <Icon />}
+                    <Text>{level ? level : name}</Text>
+                  </>
+                }
+                cardFrontBackground={color}
+                cardBackBackground={color}
+                withPadding={true}
+                brightening={true}
+              />
 
-            {description && (
-              <SkillBody variant={color}>{description}</SkillBody>
-            )}
-          </MotionSkill>
+              {description && (
+                <SkillBody variant={color}>{description}</SkillBody>
+              )}
+            </motion.div>
+          </Skill>
         ))}
       </Skills>
     </Section>
