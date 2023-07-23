f809292fc456e6daf6a4622fe8b8af597142975d
lkkolosowski <lkkolosowski@gmail.com>
2023-07-23 02:13:37 +0200

    add break lines in neccessary places

--- src/features/Homepage/Skills/Skillset/index.js
+++ src/features/Homepage/Skills/Skillset/index.js
@@ -1,62 +1,52 @@
 import Header from "../../../../common/Header";
 import Section from "../../../../common/Section";
-import Card from "../../Card";
-import { Skills, Text, Skill, SkillBody } from "./styled";
-import { motion } from "framer-motion";
+import Skill from "./Skill";
+import { Break, Skills } from "./styled";
 
 const Skillset = ({ title, icon, subtitle, skills }) => {
   return (
     <Section>
       <Header icon={icon} title={title} subtitle={subtitle} />
       <Skills>
-        {skills.map(({ name, Icon, color, level, description }, i) => (
-          <Skill>
-            <motion.div
-              initial={{
-                opacity: 0,
-                y: 75,
-                height: "100%",
-              }}
-              whileInView={{
-                opacity: 1,
-                y: 0,
-              }}
-              transition={{
-                type: "spring",
-                duration: 0.9,
-                bounce: 0.45,
-                delay: i * 0.1,
-              }}
-              viewport={{
-                once: true,
-              }}
-              key={name}
-            >
-              <Card
-                cardFrontContent={
-                  <>
-                    <Icon />
-                    <Text>{name}</Text>
-                  </>
-                }
-                cardBackContent={
-                  <>
-                    {!level && <Icon />}
-                    <Text>{level ? level : name}</Text>
-                  </>
-                }
-                cardFrontBackground={color}
-                cardBackBackground={color}
-                withPadding={true}
-                brightening={true}
-              />
-
-              {description && (
-                <SkillBody variant={color}>{description}</SkillBody>
-              )}
-            </motion.div>
-          </Skill>
-        ))}
+        {skills
+          .map(({ name, Icon, color, level, description }, i) => (
+            <Skill
+              skillIndex={i}
+              name={name}
+              Icon={Icon}
+              color={color}
+              level={level}
+              description={description}
+            />
+          ))
+          .slice(0, 3)}
+        <Break />
+        {skills
+          .map(({ name, Icon, color, level, description }, i) => (
+            <Skill
+              skillIndex={i}
+              name={name}
+              Icon={Icon}
+              color={color}
+              level={level}
+              description={description}
+            />
+          ))
+          .slice(3, 7)}
+          <Break />
+          {skills
+          .map(({ name, Icon, color, level, description }, i) => (
+            <Skill
+              skillIndex={i}
+              name={name}
+              Icon={Icon}
+              color={color}
+              level={level}
+              description={description}
+            />
+          ))
+          .slice(7)}
+          <Break />
       </Skills>
     </Section>
   );
