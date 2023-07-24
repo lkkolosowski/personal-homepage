3d9f89a078679cb609bde39cd8ef0922293a9b73
lkkolosowski <lkkolosowski@gmail.com>
2023-07-24 18:13:22 +0200

    update schema.js

--- src/features/Homepage/Contact/Form/schema.js
+++ src/features/Homepage/Contact/Form/schema.js
@@ -1,22 +1,32 @@
 import * as yup from "yup";
 
-const phoneRegExp =
+const phoneRegex =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+const stringRegex = /^(?!\s+$).*/;
 
 export const schema = yup.object().shape({
   user_name: yup
     .string()
     .min(2, "Name must be at least 2 characters")
+    .matches(stringRegex, "This field cannot contain only blankspaces")
     .required("Required"),
-  user_surname: yup.string().min(2, "Username must be at least 2 characters"),
+  user_surname: yup
+    .string()
+    .min(2, "Username must be at least 2 characters")
+    .matches(stringRegex, "This field cannot contain only blankspaces"),
   user_email: yup
     .string()
     .email("Please enter a valid email")
     .required("Required"),
   user_phone_number: yup
     .string()
-    .matches(phoneRegExp, "Please enter a valid phone number"),
-  message_topic: yup.string(),
-  message: yup.string().required("Required"),
+    .matches(phoneRegex, "Please enter a valid phone number"),
+  message_topic: yup
+    .string()
+    .matches(stringRegex, "This field cannot contain only blankspaces"),
+  message: yup
+    .string()
+    .matches(stringRegex, "This field cannot contain only blankspaces")
+    .required("Required"),
   "g-recaptcha-response": yup.string().required(),
 });
