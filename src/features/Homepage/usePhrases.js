1a133eca21b4603d8d49979dc1e05950f05d0480
lkkolosowski <lkkolosowski@gmail.com>
2023-07-26 20:26:02 +0200

    remove status check in usePhrases hook

--- src/features/Homepage/usePhrases.js
+++ src/features/Homepage/usePhrases.js
@@ -1,61 +1,54 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { selectReposStatus } from "./Portfolio/reposSlice";
 
 export const usePhrases = (phrases) => {
   const [phrase, setPhrase] = useState([]);
-  const status = useSelector(selectReposStatus);
 
   useEffect(() => {
-    if (status === "initial" || status === "loading") {
-      return;
-    } else {
-      let i = 0;
-      let j = 0;
-      let currentPhrase = [];
-      let isDeleting = false;
-      let isEnd = false;
+    let i = 0;
+    let j = 0;
+    let currentPhrase = [];
+    let isDeleting = false;
+    let isEnd = false;
 
-      const loop = () => {
-        const spedUp = 40;
-        const normalSpeed = 80;
-        const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
+    const loop = () => {
+      const spedUp = 40;
+      const normalSpeed = 80;
+      const time = isEnd ? 2000 : isDeleting ? spedUp : normalSpeed;
 
-        isEnd = false;
-        setPhrase(currentPhrase.join(""));
+      isEnd = false;
+      setPhrase(currentPhrase.join(""));
 
-        if (i < phrases.length) {
-          if (!isDeleting && j <= phrases[i].length) {
-            currentPhrase.push(phrases[i][j]);
-            j++;
-          }
-
-          if (isDeleting && j <= phrases[i].length) {
-            currentPhrase.pop(phrases[i][j]);
-            j--;
-          }
-
-          if (j === phrases[i].length) {
-            isEnd = true;
-            isDeleting = true;
-          }
-
-          if (isDeleting && j === 0) {
-            currentPhrase = [];
-            isDeleting = false;
-            i++;
-            if (i === phrases.length) {
-              i = 0;
-            }
-          }
+      if (i < phrases.length) {
+        if (!isDeleting && j <= phrases[i].length) {
+          currentPhrase.push(phrases[i][j]);
+          j++;
         }
 
-        setTimeout(loop, time);
-      };
+        if (isDeleting && j <= phrases[i].length) {
+          currentPhrase.pop(phrases[i][j]);
+          j--;
+        }
 
-      loop();
-    }
-  }, [phrases, status]);
+        if (j === phrases[i].length) {
+          isEnd = true;
+          isDeleting = true;
+        }
+
+        if (isDeleting && j === 0) {
+          currentPhrase = [];
+          isDeleting = false;
+          i++;
+          if (i === phrases.length) {
+            i = 0;
+          }
+        }
+      }
+
+      setTimeout(loop, time);
+    };
+
+    loop();
+  }, [phrases]);
 
   return phrase;
 };
