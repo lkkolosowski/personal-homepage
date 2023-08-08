7736655649d1252015c67ea1cf6812b37a3016e4
lkkolosowski <lkkolosowski@gmail.com>
2023-08-09 00:25:17 +0200

    preload images

--- src/features/Homepage/Portfolio/Content/index.js
+++ src/features/Homepage/Portfolio/Content/index.js
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import { projects } from "./projects";
 import Repositories from "./Repositories";
 import Loader from "./Loader";
 import Failure from "./Failure";
 
 const Content = ({ status, repositories }) => {
+  const loadImages = () => {
+    projects.forEach(({ thumbnail }) => {
+      new Image().src = thumbnail;
+    });
+  };
+
+  useEffect(() => {
+    loadImages();
+  }, []); // eslint-disable-line
+
   switch (status) {
     case "initial":
       return null;
@@ -14,7 +26,7 @@ const Content = ({ status, repositories }) => {
       return <Failure />;
 
     case "success":
-      return <Repositories repositories={repositories} />;
+      return <Repositories projects={projects} repositories={repositories} />;
 
     default:
       throw new Error(`Incorrect status: ${status}`);
