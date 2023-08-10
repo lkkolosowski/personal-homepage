fdd646ad5eca6ac63079780b38a4c6025ad93168
lkkolosowski <lkkolosowski@gmail.com>
2023-08-10 15:35:30 +0200

    add reveal animation to error component

--- src/features/Homepage/Portfolio/Content/Failure/index.js
+++ src/features/Homepage/Portfolio/Content/Failure/index.js
@@ -1,9 +1,15 @@
 import { ButtonLink } from "../../../../../common/Link";
 import { links } from "../../../nameplate";
+import { motion } from "framer-motion";
 import { Wrapper, Title, Paragraph, Icon } from "./styled";
 
 const Failure = () => (
-  <Wrapper>
+  <Wrapper
+    as={motion.div}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    transition={{ duration: 0.75 }}
+  >
     <Icon />
     <Title>Ooops! Something went wrong...</Title>
     <Paragraph>
